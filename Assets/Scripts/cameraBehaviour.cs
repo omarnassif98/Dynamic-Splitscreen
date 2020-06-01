171d7efcf3d54b0b9f430ac1ab933789bd137ec0
omarnassif98 <33793027+omarnassif98@users.noreply.github.com>
2020-05-31 20:07:54 -0400

    Cleaned and documented code

--- Assets/Scripts/cameraBehaviour.cs
+++ Assets/Scripts/cameraBehaviour.cs
@@ -4,10 +4,7 @@ using System.Collections;
 
 public class cameraBehaviour : MonoBehaviour {
 
-	bool onePlayer;
-
-	public Transform player1;
-	public Transform player2;
+	public Transform player1, player2; 
 
 	public float splitDistance = 5;
 
@@ -28,22 +25,25 @@ public class cameraBehaviour : MonoBehaviour {
 	Vector3 margin2;
 
 	void Start () {
-
-		if (player1 != null && player2 != null) {
-			onePlayer = false;
-		} else {
-			onePlayer = true;
-		}
+		//Setting up cameras
 		camera1 = gameObject;
 		camera2 = new GameObject ();
 		camera2.AddComponent<Camera> ();
-		camera2.GetComponent<Camera> ().cullingMask = ~(1 << LayerMask.NameToLayer ("TransparentFX"));
-
+		
+		/*
+		The splitter is essentially a square infront of the
+		camera that splits the screen in two with a dynamic axis
+		if the two player objects are far enough from one another.
+		The square has a simple stencil shader attached to it
+		that simply skips rendering the area occupied by it.
+		Effectively, the area in question is 'transparent'. As such
+		camera 2, which completely ignores the square is
+		rendered in that space. 
+		*/
 		splitter = new GameObject();
-		splitter.transform.parent = gameObject.transform;
+		splitter.transform.parent = transform;
 		splitter.transform.localPosition = Vector3.forward;
 		splitter.SetActive (false);
-
 		split = GameObject.CreatePrimitive (PrimitiveType.Quad);
 		split.transform.parent = splitter.transform;
 		split.transform.localPosition = new Vector3 (0,-3.5f,0);
@@ -54,60 +54,64 @@ public class cameraBehaviour : MonoBehaviour {
 		Material tempMat2 = new Material (Shader.Find ("Omar/Splitscreen"));
 		split.GetComponent<Renderer>().material = tempMat2;
 		split.layer = LayerMask.NameToLayer ("TransparentFX");
+		camera2.GetComponent<Camera> ().cullingMask = ~(1 << LayerMask.NameToLayer ("TransparentFX"));
 	}
 	float angle;
 	void LateUpdate () {
-		if (onePlayer == false) {
+			
 			float zDistance = player1.position.z - player2.transform.position.z;
 			distance = Vector3.Distance (player1.position, player2.transform.position);
-
-
-			if (player1.transform.position.x <= player2.transform.position.x) {
-				angle = Mathf.Rad2Deg * Mathf.Acos (zDistance / distance);
-			} else {
-				angle = Mathf.Rad2Deg * Mathf.Asin (zDistance / distance) - 90;
-			}
-
-
-			splitter.transform.localEulerAngles = new Vector3 (0, 0, angle);
-
-			Vector3 midPoint = new Vector3 ((player1.position.x + player2.position.x) / 2, (player1.position.y + player2.position.y) / 2, (player1.position.z + player2.position.z) / 2); 
-			if (camera1.GetComponent<Camera> ().WorldToViewportPoint (player2.position).x > 1 - delta || camera1.GetComponent<Camera> ().WorldToViewportPoint (player2.position).x < 0 + delta || camera1.GetComponent<Camera> ().WorldToViewportPoint (player2.position).y > 1 - delta || camera1.GetComponent<Camera> ().WorldToViewportPoint (player2.position).y < 0 + delta || camera1.GetComponent<Camera> ().WorldToViewportPoint (player1.position).x > 1 - delta || camera1.GetComponent<Camera> ().WorldToViewportPoint (player1.position).x < 0 + delta || camera1.GetComponent<Camera> ().WorldToViewportPoint (player1.position).y > 1 - delta || camera1.GetComponent<Camera> ().WorldToViewportPoint (player1.position).y < 0 + delta) {
-				oneView = false;
-			}
 			if (distance < 15) {
 				oneView = true;
+			}else{
+				oneView = false;
 			}
-			//Waits for the two cameras to split and then calcuates a midpoint relevant to the difference in position between the two cameras.
+			
+			
+
+
+			/*
+			The camera focuses on the midpoint of the two players if they are 
+			close enough to be shown in the same frame. If not, the cameras point at offsets in
+			the direction of the other player so that the two players occupy the appropriate
+			portions of the bisected screen.
+			*/
+			Vector3 midPoint = new Vector3 ((player1.position.x + player2.position.x) / 2, (player1.position.y + player2.position.y) / 2, (player1.position.z + player2.position.z) / 2); 
 			if (oneView == false) {
+				//decides what angle to bisect the screen
+				//Since it is always a portion of camera 1 which is masked,
+				//the calculations to find the angle are different whether player one
+				//is to the left or right of player 2
+				if (player1.transform.position.x <= player2.transform.position.x) {
+					angle = Mathf.Rad2Deg * Mathf.Acos (zDistance / distance);
+				} else {
+					angle = Mathf.Rad2Deg * Mathf.Asin (zDistance / distance) - 90;
+				}
+
+				splitter.transform.localEulerAngles = new Vector3 (0, 0, angle);
 				Vector3 offset = midPoint - player1.position; 
-				offset.x = Mathf.Clamp (offset.x, -splitDistance, splitDistance);
-				offset.y = Mathf.Clamp (offset.y, -splitDistance, splitDistance);
-				offset.z = Mathf.Clamp (offset.z, -splitDistance, splitDistance);
+				offset = offset.normalized * splitDistance;
 				midPoint = player1.position + offset;
 
 				Vector3 offset2 = midPoint - player2.position; 
-				offset2.x = Mathf.Clamp (offset2.x, -splitDistance, splitDistance);
-				offset2.y = Mathf.Clamp (offset2.y, -splitDistance, splitDistance);
-				offset2.z = Mathf.Clamp (offset2.z, -splitDistance, splitDistance);
+				offset2 = offset2.normalized * splitDistance;
 				Vector3 midPoint2 = player2.position - offset;
 
-
+				//If it's the first frame rendering in two views, snap the cameras in place
+				//otherwise have them smoothly glide to their destinations
 				if (splitter.activeSelf == false) {
 					splitter.SetActive (true);
 					camera2.SetActive (true);
 					camera2.transform.position = camera1.transform.position;
 					camera2.transform.rotation = camera1.transform.rotation;
-
 				} else {
 					camera2.transform.position = Vector3.Lerp (camera2.transform.position, midPoint2 + margin2, Time.deltaTime * 1.5f);
 					Quaternion newRot2 = Quaternion.LookRotation (midPoint2 - camera2.transform.position);
 					camera2.transform.rotation = Quaternion.Lerp (camera2.transform.rotation, newRot2, Time.deltaTime * 1.5f);
 				}
 
-			} else {
-				if (splitter.activeSelf)
-					splitter.SetActive (false);
+			} else {	
+				splitter.SetActive (false);
 				camera2.SetActive (false);
 			}
 
@@ -118,16 +122,11 @@ public class cameraBehaviour : MonoBehaviour {
 			}
 			Quaternion newRot = Quaternion.LookRotation (midPoint - camera1.transform.position);
 			camera1.transform.rotation = Quaternion.Lerp (camera1.transform.rotation, newRot, Time.deltaTime * 1.5f);
-		} else {
-			camera1.transform.position = Vector3.Lerp (camera1.transform.position, player1.position + margin, Time.deltaTime * 1.5f);
-			Quaternion newRot = Quaternion.LookRotation (player1.position - camera1.transform.position);
-			camera1.transform.rotation = Quaternion.Lerp (camera1.transform.rotation, newRot, Time.deltaTime * 1.5f);
-
-		}
 	}
 
 	void OnGUI(){
-		if (oneView == false && onePlayer == false) {
+		//draws a bar between the axis bisecting the screen
+		if (oneView == false) {
 			GUIUtility.RotateAroundPivot (-angle, new Vector2 (Screen.width / 2, Screen.height / 2));
 			GUI.DrawTexture (new Rect (Screen.width / 2, Screen.height / 2, 222222, 13), splitterGraphic);
 			GUI.DrawTexture (new Rect (Screen.width / 2, Screen.height / 2, -222222, 13), splitterGraphic);
